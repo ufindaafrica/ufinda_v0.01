0361080d724ec13d664f786772ff18ca61b5b122
ellalearns <ellalearnscopy@gmail.com>
2025-10-08 14:39:27 +0100

    set up styling

---
+++ styles/globals.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globals = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "green"
+    },
+    text: {
+        fontFamily: "System",
+        fontSize: 30
+    }
+});
