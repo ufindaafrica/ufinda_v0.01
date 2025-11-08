dc753e35131c7f8886c68c960929fbe9f1d7550f
Saro <sarobaridoo@gmail.com>
2025-11-08 07:49:17 +0100

    Create jest.config.js

---
+++ landingpage/jest.config.js
@@ -0,0 +1,8 @@
+export default {
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!axios|lucide-react|react-router-dom)/",
+  ],
+};
