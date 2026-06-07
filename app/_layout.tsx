20977f92598cf474b1770a577cd7b7171a448575
ellalearns <ellalearnscopy@gmail.com>
2026-06-07 11:59:03 +0100

    finish notifications

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,18 @@
 import { WebSocketProvider } from "@/contexts/chatSocket"
 import { Stack } from "expo-router";
 import { GestureHandlerRootView} from "react-native-gesture-handler"
+import * as Notifications from "expo-notifications"
 
 export default function RootLayout() {
+
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowBanner: true,
+      shouldShowList: true,
+      shouldPlaySound: true,
+      shouldSetBadge: true
+    })
+  })
 
   return <WebSocketProvider>
     <GestureHandlerRootView style={{ flex: 1}}>
