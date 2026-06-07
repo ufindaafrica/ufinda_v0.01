738b4896e5c2ab1025db50b21bf6d67612cf38d6
ellalearns <ellalearnscopy@gmail.com>
2026-06-07 12:50:57 +0100

    the search area for shop

--- app/shop/shop.tsx
+++ app/shop/shop.tsx
@@ -1,9 +1,11 @@
 import AppHeader from "@/components/appHeader";
+import Search from "@/components/search";
 import { images } from "@/constants/images";
 import { globals } from "@/styles/globals";
 import { homeStyles } from "@/styles/home";
-import { Image, View } from "react-native";
+import { Image, TouchableOpacity, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { SearchBar } from "react-native-screens";
 
 
 export default function Shop() {
@@ -18,7 +20,19 @@ export default function Shop() {
                     <Image source={images.comingSoon} style={{ width: 243, height: 243 }} />
                 </View> */}
 
-                
+                <View style={[homeStyles.layoutMargin, {flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center'}]}>
+                    <View style={{width: "85%"}}>
+                        <Search />
+                    </View>
+                    <TouchableOpacity style={{width: 44, height: 44, backgroundColor: "#ffffff", borderRadius: 30, justifyContent: 'center', alignItems: 'center'}}>
+                        <Image source={images.sort} style={{width: 24, height: 24}} />
+                    </TouchableOpacity>
+                </View>
+
+                {/* category list view */}
+                <View></View>
+
+
             </SafeAreaView>
         </SafeAreaProvider>
     )
